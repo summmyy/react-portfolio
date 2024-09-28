9bb2df467607e9a67d5c8dcde0625055535631c3
Summy <Sunmibolaj13@gmail.com>
2024-09-28 12:59:45 -0400

    ran build commands

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Heading,
-    Box,
-    Text,
-    VStack
 
- } from '@chakra-ui/react';
 import LandingSection from '../components/LandingSection';
 
 const About = () => {
